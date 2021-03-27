2f85cd08d816f6ee44a471e443f3a0b7f389b5cc
Lauro Costa <joaolauro94@gmail.com>
2021-03-27 00:32:19 -0300

    created hooks transactions

--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode} from "react";
-import { api } from "./services/api";
+import { createContext, useEffect, useState, ReactNode, useContext} from "react";
+import { api } from "../services/api";
 
 interface Transaction {
   id: number;
@@ -18,10 +18,10 @@ interface TransactionsProvidersProps {
 
 interface TransactionsContextData {
   transactions:  Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -35,8 +35,18 @@ export function TransactionsProvider({ children }: TransactionsProvidersProps) {
   }, []);
 
 
-  function createTransaction(transaction: TransactionInput){
-    api.post('/transactions', transaction);
+  async function createTransaction(transactionInput: TransactionInput){
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction,
+    ])
   }
 
   return (
@@ -44,4 +54,9 @@ export function TransactionsProvider({ children }: TransactionsProvidersProps) {
       {children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions() {
+  const context = useContext( TransactionsContext );  
+  return context;
 }

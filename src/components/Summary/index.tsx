708e78883dced20060da966eab18a100d649c686
Lauro Costa <joaolauro94@gmail.com>
2021-03-26 23:35:48 -0300

    created transactions context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,14 +2,20 @@ import { Container } from "./style";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import total from "../../assets/total.svg";
+import { useContext } from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 
 export function Summary() {
+
+  const {transactions} = useContext(TransactionsContext);
+  console.log(transactions);
+
   return (
     <Container>
       <div>
         <header>
           <p>Entradas</p>
-          <img src={incomeImg} alt="Entradas"/>
+          <img src={incomeImg} alt="Entradas"/>  
         </header>
         <strong>R$1000,00</strong>
       </div>
